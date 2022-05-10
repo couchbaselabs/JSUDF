38264f11515c87c3fae9fb2a30612096ae3a83ca
kaminijagtiani <56046223+kaminijagtiani@users.noreply.github.com>
2022-05-09 18:22:12 -0700

    PL/SQL to JSUDF for PL/SQL function removing explicit close()

--- example8.js
+++ example8.js
@@ -23,33 +23,19 @@ cbq> SELECT count(*) FROM emp;
 
 function insertdata(){
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"KING","EMPNO":"7839","SAL":"5000"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"SCOTT","EMPNO":"7788","SAL":"3000"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"FORD","EMPNO":"7902","SAL":"3000"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"JONES","EMPNO":"7566","SAL":"2975"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"BLAKE","EMPNO":"7698","SAL":"2850"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"CLARK","EMPNO":"7782","SAL":"2450"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"ALLEN","EMPNO":"7499","SAL":"1600"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"TURNER","EMPNO":"7844","SAL":"1500"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"MILLER","EMPNO":"7934","SAL":"1300"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"WARD","EMPNO":"7521","SAL":"1250"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"MARTIN","EMPNO":"7654","SAL":"1250"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"ADAMS","EMPNO":"7876","SAL":"1100"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"JAMES","EMPNO":"7900","SAL":"950"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"ENAME":"SMITH","EMPNO":"7369","SAL":"800"});
-insertquery.close();
 }
 
 function totalEmployees(){
