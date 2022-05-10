d961d8741deaeb926f0d138561bfb35df0893f1f
kaminijagtiani <56046223+kaminijagtiani@users.noreply.github.com>
2022-05-09 17:32:54 -0700

    An example with insert,update,delete,select- removing explicit close()

--- example5.js
+++ example5.js
@@ -40,37 +40,24 @@ var l_salary=0;
 var l_manager;
 
 var querybegin=BEGIN WORK;
-querybegin.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"emp_name":"BBB","emp_no":"1000","salary":"25000","manager":"AAA"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"emp_name":"XXX","emp_no":"1001","salary":"10000","manager":"BBB"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"emp_name":"YYY","emp_no":"1002","salary":"10000","manager":"BBB"});
-insertquery.close();
 var insertquery = INSERT INTO emp VALUES (uuid(),{"emp_name":"ZZZ","emp_no":"1003","salary":"7500","manager":"BBB"});
-insertquery.close();
 var query = COMMIT WORK;
-query.close();
-
 
 var querybegin=BEGIN WORK;
-querybegin.close();
 var updatequery = UPDATE emp SET salary=15000 WHERE emp_name='XXX';
 var query = COMMIT WORK;
-query.close();
 
 var querybegin=BEGIN WORK;
-querybegin.close();
 var updatequery = DELETE from emp WHERE emp_name='ZZZ';
 var query = COMMIT WORK;
-query.close();
 
 var selectquery =SELECT emp_name,emp_no,salary,manager FROM emp WHERE emp_name='XXX';
 var rs = [];
 for (const row of selectquery) {
     rs.push(row);
     }
-return rs
-
-
+return rs;
 }
